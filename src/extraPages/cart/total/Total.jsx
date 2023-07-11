6f74e0773273a72bbfccd32cac2091a16a826113
aadimr <adityashaw9066@gmail.com>
2023-07-11 21:04:53 +0530

    payment button functionality is done

--- src/extraPages/cart/total/Total.jsx
+++ src/extraPages/cart/total/Total.jsx
@@ -3,6 +3,9 @@ import Buttons from "../../../components/button/Button"
 import { showUser } from "../../../store/UserSlice"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { updateUserCartItemQuantity } from "../../../store/UserSlice"
 
 function Total() {
 
@@ -21,8 +24,29 @@ function Total() {
   const cartItemAmt = showLoggedInUser.addCart
   const totalAmount = cartItemAmt.length ? cartItemAmt.map(ele => ele.price*ele.quantity).reduce((x, y) => +x + +y):null;
 
+  const payNotify = () => (toast.success('Your order has been placed', {
+    position: "top-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "light",
+  }))
+  console.log(showLoggedInUser)
+  function handleClickPay(){
+      const updateUser = {
+          ...showLoggedInUser,
+          addCart:[]
+      } 
+      console.log(updateUser)
+      dispatch(updateUserCartItemQuantity(updateUser))
+      payNotify()
+  }
+
   return (
     <div className={style.wrapper}>
+       <ToastContainer />
       <h2 className={style.header}>Order Now</h2>
       <div className={style.subTotalAndItem}>
         <p>ITEMS</p>
@@ -47,7 +71,7 @@ function Total() {
         },
       }}
       name={"Pay now"}
-      />
+      onClick={handleClickPay}/>
     </div>
   )
 }
--- src/newsletter/Newsletter.jsx
+++ src/newsletter/Newsletter.jsx
@@ -1,18 +1,33 @@
-import Buttons from "../components/button/Button"; 
+import Buttons from "../components/button/Button";
 import Input from "../components/input/Input";
 import style from "./Newsletter.module.css"
+import { useState } from "react";
 
 function Newsletter() {
+
+  const [val, setVal] = useState("")
+
+  function handleOnChange(e) {
+    setVal(e.target.value)
+  }
+
+  function handleOnClick() {
+    if (val.trim().length === 0){
+        
+    }
+      setVal("")
+  }
+
   return (
     <div className={style.wrapper}>
-      <div className={style.img}><img src="https://protonpiston.com/katalog/assets/images/background/bg1.jpg" alt='error' className={style.img}/></div>
+      <div className={style.img}><img src="https://protonpiston.com/katalog/assets/images/background/bg1.jpg" alt='error' className={style.img} /></div>
       <div className={style.textOverlay}>
-      <h1>Join Newsletter</h1>
-      <p className={style.p}>Lorem started its journey with cast iron (CI) products in 1980. The initial main objective was to ensure pure water and affordable irrigation.</p>
-      <div className={style.inputAndButton}>
-      <Input className={style.input} placeholder={"Enter your email"}/>
-      <Buttons name={"Subscribe"} sx={{borderRadius:"2rem", height:"3rem", textTransform:"capitalize", fontSize:"1.2rem"}}/>
-      </div>
+        <h1>Join Newsletter</h1>
+        <p className={style.p}>Be the first to know about new releases, exclusive discounts, and more.</p>
+        <div className={style.inputAndButton}>
+          <Input className={style.input} placeholder={"Enter your email"} onChange={handleOnChange} value={val}/>
+          <Buttons onClick={handleOnClick} type={"submit"} name={"Subscribe"} sx={{ borderRadius: "2rem", height: "3rem", textTransform: "capitalize", fontSize: "1.2rem" }} />
+        </div>
       </div>
     </div>
   )
