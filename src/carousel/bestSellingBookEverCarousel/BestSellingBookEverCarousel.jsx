cc07c1b40e67558c84efbfcdd2cedccdc52ea1db
aadimr <adityashaw9066@gmail.com>
2023-07-13 20:27:50 +0530

    home page responsiveness is done

--- src/carousel/bestSellingBookEverCarousel/BestSellingBookEverCarousel.jsx
+++ src/carousel/bestSellingBookEverCarousel/BestSellingBookEverCarousel.jsx
@@ -1,5 +1,5 @@
 import style from "./BestSellingBookEverCarousel.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { showBook } from "../../store/BooksSlice";
 import StarIcon from '@mui/icons-material/Star';
@@ -11,6 +11,36 @@ import LeftArrow from '../carouselArrows/LeftArrow';
 import RightArrow from '../carouselArrows/RightArrow';
 
 function BestSellingBookEverCarousel() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  let slidesToShow = 5;
+
+  if (windowWidth <= 1170) {
+    slidesToShow = 4;
+  }
+  if (windowWidth <= 850) {
+    slidesToShow = 3;
+  }
+  if (windowWidth <= 620){
+    slidesToShow = 2;
+  }
+  if (windowWidth <= 400){
+    slidesToShow = 1;
+  }
+
   const dispatch = useDispatch();
 
   const { books, loading } = useSelector((state) => state.app);
@@ -23,11 +53,13 @@ function BestSellingBookEverCarousel() {
     return <h2>Loading...</h2>
   }
 
+
+
   const settings = {
     dots: false,
     infinite: true,
     speed: 1000,
-    slidesToShow: 5,
+    slidesToShow: slidesToShow,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
@@ -47,13 +79,53 @@ function BestSellingBookEverCarousel() {
           {filteredBook && filteredBook
             .map((ele, index) => (
               <Card sx={{
-                 maxWidth: "12rem",
-                 maxHeight: "28rem",
-                 '@media screen and (max-width: 1200px)': {
+                maxWidth: "12rem",
+                maxHeight: "28rem",
+                '@media screen and (max-width: 1250px)': {
+                  maxWidth: "11rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 1170px)': {
+                  maxWidth: "13rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 1075px)': {
+                  maxWidth: "11rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 935px)': {
                   maxWidth: "10rem",
                   maxHeight: "28rem",
                 },
-                 }} key={index}>
+                '@media screen and (max-width: 850px)': {
+                  maxWidth: "12rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 750px)': {
+                  maxWidth: "11rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 690px)': {
+                  maxWidth: "10rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 620px)': {
+                  maxWidth: "13rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 535px)': {
+                  maxWidth: "11rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 450px)': {
+                  maxWidth: "10rem",
+                  maxHeight: "28rem",
+                },
+                '@media screen and (max-width: 400px)': {
+                  maxWidth: "18rem",
+                  maxHeight: "28rem",
+                },
+              }} key={index}>
                 <img src={ele.img} alt="error" className={style.img} />
                 <div className={style.details}>
                   <p className={style.book_Name}>{ele.book_Name}</p>
@@ -62,7 +134,7 @@ function BestSellingBookEverCarousel() {
                     <div className={style.reviewStar}>
                       <p>
                         {ele.review}  </p>
-                      <span><StarIcon sx={{fontSize:"medium",marginTop:".3rem",color:"#FFD700"}}/></span>
+                      <span><StarIcon sx={{ fontSize: "medium", marginTop: ".3rem", color: "#FFD700" }} /></span>
                     </div>
                     <h3 className={style.h3}>₹{ele.price}</h3>
                   </div>
